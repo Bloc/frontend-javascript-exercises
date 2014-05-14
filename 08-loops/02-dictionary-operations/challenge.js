5dfb7f755a3fca265d72d5c05e0829dcaa079ea3
Robert Smolinski <rob@bloc.io>
2014-05-14 10:11:12 -0700

    Added dictionary looping challenges. Removed separate dictionary checkpoint because it should be covered earlier.

---
+++ 08-loops/02-dictionary-operations/challenge.js
@@ -0,0 +1,46 @@
+
+// TODO: Complete the `copy` function
+// copy should:
+//   - Take one dictionary as an argument.
+//   - Create a new dictionary and copy all of the properties of the input to the new dictionary.
+//   - Return the new dictionary.
+//
+// Ex. copy({name: "Rob"}) returns a new dictionary {name: "Rob"}
+module.exports.copy = function(src) {
+  var dest = {};
+  for (k in src) {
+    dest[k] = src[k];
+  }
+  return dest;
+};
+
+// TODO: Complete the `extend` function
+// extend should:
+//   - Take two dictionaries as arguments (`dest` and `src`) and copy all of the properties (e.g. key, value pairs) of `src` into `dest`.
+//   - Return a modified `dest`.
+//
+// Ex. extend({name: "Rob"}, {age: 101}) should modify and return `dest` with content of {name: "Rob", age: 101}
+// Ex. extend({name: "Rob"}, {name: "Steve"}) should modify and return `dest` with content of {name: "Steve"}
+//
+module.exports.extend = function(dest, src) {
+  for (k in src) {
+    dest[k] = src[k];
+  }
+  return dest;
+};
+
+// TODO: Complete the `hasElems` function.
+// hasKey should:
+//   - Take two arguments the first is a dictionary and the second argument is an array of elements.
+//   - This function should return true when all of the elements in the array
+//      are keys in the dictionary, false otherwise.
+//
+module.exports.hasElems = function(dict, elems) {
+  for (var i = 0; i < elems.length; i++) {
+    if (dict[elems[i]] === undefined) {
+      return false;
+    }
+  }
+  return true;
+};
+
---
+++ 08-loops/02-dictionary-operations/spec.js
@@ -0,0 +1,55 @@
+var expect = require('chai').expect;
+var solution = require('./challenge');
+var _ = require('underscore');
+
+describe("copy", function() {
+  it("should create a new dictionary with a copy of all the data", function() {
+    orig = {name: "Rob"};
+    result = solution.copy(orig);
+    expect(result).to.not.equal(orig);
+    expect(result).to.deep.eql(orig);
+  });
+});
+
+describe("extend", function() {
+  it("should correctly extends two hashes that are unique", function() {
+    dest = { name: "Computer", cost: "$1,000" };
+    src = { first_name: "Bob", age: 34 };
+    result = solution.extend(dest, src);
+
+    expect(result).to.equal(dest, "Returned hash must be the same as the original destination hash.");
+    expect(result).to.have.keys(['name', 'cost', 'first_name', 'age']);
+  });
+
+  it("should correctly extend two hashes that are have keys in common", function() {
+    dest = { name: "Computer", cost: "$1,000" };
+    src = { name: "Mouse", uuid: "1234" };
+    result = solution.extend(dest, src);
+
+    expect(result).to.have.keys(['name', 'cost', 'uuid']);
+    expect(result['name']).to.eql('Mouse');
+  });
+});
+
+
+describe("hasElems", function() {
+  var h = {};
+  beforeEach(function() {
+    h = { name: "Computer", cost: "$1,000" };
+  });
+
+  it("returns true for an empty array", function() {
+    expect(solution.hasElems(h, [])).to.be.true;
+  });
+
+
+  it("returns true if it has all the keys", function() {
+    keys = ['name', 'name', 'name']
+    expect(solution.hasElems(h, keys)).to.be.true;
+  });
+
+  it("returns false if one or more of the keys isn't in the hash", function() {
+    var keys = ['name', 'bio', 'cost'];
+    expect(solution.hasElems(h, keys)).to.be.false;
+  });
+});
