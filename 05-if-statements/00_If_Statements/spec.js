d497972aa0214d42216c6865c73cea4e8c6e0914
Roshan Choxi <roshan.choxi@gmail.com>
2014-05-14 11:56:25 -0700

    restructure directories

--- 05-if-statements/00_If_Statements/spec.js
+++ 05-if-statements/00_If_Statements/spec.js
@@ -1,16 +1,16 @@
-var assert    = require("assert");
+var expect    = require("chai").expect;
 var challenge = require("./challenge.js");
 
 describe("favorite_number", function() {
   it("should return 'Too low' if the guess is low", function() {
-    assert.equal(challenge.favorite_number(10, 1), "Too low");
+    expect(challenge.favorite_number(10, 1)).to.eql("Too low");
   });
 
   it("should return 'Too high' if the guess is high", function() {
-    assert.equal(challenge.favorite_number(5, 11), "Too high");
+    expect(challenge.favorite_number(5, 11)).to.eql("Too high");
   });
 
   it("should return 'You got it!' if the guess is right", function() {
-    assert.equal(challenge.favorite_number(11, 11), "You got it!");
+    expect(challenge.favorite_number(11, 11)).to.eql("You got it!");
   });
 });
