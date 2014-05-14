5efbfaec16e067764264da1bac7e9fe0dc34f6a5
Mike Jewett <mikesjewett@gmail.com>
2014-05-13 22:05:18 -0400

    Removed example and syntax. No exercises associated with these

--- 02-reading-rspec-tests/00_A_Tested_Method/spec.rb
+++
@@ -1,9 +0,0 @@
-describe "link_to" do
-  it "should return a valid link for Bloc" do
-    link_to("Bloc", "http://www.bloc.io").should eq("<a href='http://www.bloc.io'>Bloc</a>")
-  end
-  it "should return a valid link for Google" do
-    link_to("Google", "http://www.google.com").should eq("<a href='http://www.google.com'>Google</a>")
-  end
-end
-
