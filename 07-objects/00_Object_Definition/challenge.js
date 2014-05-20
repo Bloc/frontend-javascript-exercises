c21aa04d44bfd4c9f94528aa7c2a2cd2ab17d9d0
Joe Lipper <lipperjj@gmail.com>
2014-05-20 12:40:19 -0700

    mocha tests aren't passing. committing for review.

--- 07-objects/00_Object_Definition/challenge.js
+++ 07-objects/00_Object_Definition/challenge.js
@@ -11,6 +11,24 @@ module.exports.createCourse = function(courseTitle, courseDuration, courseStuden
 		title: courseTitle,
 		duration: courseDuration,
 		students: courseStudents
-	};
+	}
 	return course;
+};
+
+// TODO: write a function called addProperty
+// addProp should:
+// 		- take three arguments: an object to add a property to, a property name and its value
+// 		- if the property is defined, the function should console.log "property already exists"
+// 		- hasOwnProperty method can check if you pass string of property
+// 		-	return the new object
+// Ex. var student = {school: 'Berkeley High School', lastName: 'Smith'};
+// addProperty(student, 'firstName', 'Jim') should return 
+// {school: 'Berkeley High School', lastName: 'Smith', firstName: 'Jim'}
+module.exports.addProperty = function(object, newProp, newValue) {
+	if (object.newProp) {
+		console.log("Property already exists.");
+	} else {
+		object[newProp] = newValue;
+		return object;
+	}
 };
--- 07-objects/00_Object_Definition/spec.js
+++ 07-objects/00_Object_Definition/spec.js
@@ -1,15 +1,38 @@
 var expect    = require("chai").expect;
 var challenge = require("./challenge.js");
 
+var course = {
+	title: "Full Stack Engineering",
+	duration: "4 weeks",
+	students: ["Rob", "Tim", "Joe"]
+};
+
+var title = "Full Stack Engineering",
+		duration = "4 weeks",
+		students = ["Rob", "Tim", "Joe"];
+
 describe("createCourse", function() {
 	it("creates a course with a title, duration and student list", function() {
-		name = "Full Stack Engineering";
-		duration = "4 weeks";
-		students = ["Rob", "Tim", "Joe"];
-		expect(challenge.createCourse(name, duration, students)).to.equal({
-			name: "Full Stack Engineering",
-			duration: "4 weeks",
-			students: ["Rob", "Tim", "Joe"]
+		expect(challenge.createCourse(title, duration, students)).to.equal(course);
+	});
+});
+
+var teacher = {subject: 'Algebra 2', school: 'Berkeley High School'};
+
+var teacherWithProp = {subject: 'Algebra 2', school: 'Berkeley High School', favoriteStudent: 'Mark'};
+
+describe("addProperty", function() {
+	it("adds a property to an existing object", function() {
+		expect(challenge.addProperty(teacher, 'favoriteStudent', 'Mark')).to.equal({
+			subject: 'Algebra 2',
+			school: 'Berkeley High School',
+			favoriteStudent: 'Mark'
+		});
+	});
+
+	it("sould notify us when a property already exists", function() {
+		expect(challenge.addProperty(teacherWithProp, 'favoriteStudent', 'Mark')).to.equal({
+			console.log("Property already exists.");
 		});
 	});
 });
