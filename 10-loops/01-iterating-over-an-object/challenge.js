ad52d20c91419ebb4c4926fbcc8912adc5ac367b
Mike Jewett <mikesjewett@gmail.com>
2014-05-24 14:20:20 -0400

    Updated iterating over an object

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,7 +1,3 @@
-// TODO: Complete the `getKeys` function
-// getKeys should:
-//   - Return an array of each of the keys in the object.
-// Ex. getKeys({name: "Rob", age: 101}) returns ["name", "age"]
 module.exports.getKeys = function(object) {
   output = [];
   for (var k in object) {
@@ -10,10 +6,6 @@ module.exports.getKeys = function(object) {
   return output;
 }
 
-// TODO: Complete the `getValues` function
-// getValues should:
-//   - Return an array of each of the values in the object.
-// Ex. getValues({name: "Rob", age: 101}) returns ["Rob", 101]
 module.exports.getValues = function(object) {
   output = [];
   for (var k in object) {
@@ -22,14 +14,6 @@ module.exports.getValues = function(object) {
   return output;
 }
 
-
-// TODO: Complete the `objectToArray` function
-// objectToArray should:
-//   - Convert the object passed in to an array of strings.
-//   - Each string should be in the format of "[key] is [value]" for each key/value pair in the object.
-//   - If an empty object is passed in, you should return an empty array.
-//
-// Ex. objectToArray({name: "Rob"}) should return ["name is Rob"]
 module.exports.objectToArray = function(object) {
   output = [];
   for (k in object) {
