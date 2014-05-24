53b878545ab477bab0fd293942568518b3d91334
Mike Jewett <mikesjewett@gmail.com>
2014-05-24 12:40:21 -0400

    Updated debugging

---
+++ 03-debugging-code/00_Reference_Error/README.md
@@ -0,0 +1,11 @@
+The "reference error" is one of the easiest errors to diagnose. That being said, it's worth practicing because it's important to see what the errors look like in the context of Mocha.
+
+Don't type anything - just run `mocha spec.js` from your terminal. You should see something that looks like:
+
+```
+ReferenceError: hello is not defined
+```
+
+Mocha tried to run invoke the `hello` function but it couldn't find the function definition, because we haven't defined it yet.
+
+Now that you know what the error is caused by, uncomment out the code in `challenge.js` and write a function that will pass the tests. Running `mocha spec.js `in the `03-debugging-code/00_Reference_Error` directory should show that you have two passing test now.
--- 03-debugging-code/00_Reference_Error/challenge.js
+++ 03-debugging-code/00_Reference_Error/challenge.js
@@ -1,19 +1,3 @@
-// This is the easiest error to diagnose, but it's still worth practicing
-// because it's important to see what the errors look like in the context of
-// Mocha.
-
-// Don't type anything - just run "mocha spec.js". You should see something that looks
-// like:
-
-// ReferenceError: hello is not defined
-
-// This means that Mocha tried to run invoke the `hello` function but it couldn't
-// find the function definition (because we haven't defined it yet).
-
-// Now that you know what the error is caused by, uncomment out the below code to define
-// a function that will pass the tests. Running "mocha spec.js" in this directory will show
-// that you have 2 passing test now.
-//
 // module.exports.hello = function(msg) {
 //   return "Hello " + msg;
 // };
