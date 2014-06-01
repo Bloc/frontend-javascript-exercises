eb31a0a5afe10a974bc405bd862995c24e0952a5
Robert Smolinski <rob@bloc.io>
2014-06-01 13:34:16 -0700

    Changed challenges for the while loop exerciess.

--- 10-loops/03-while-loops/spec.js
+++ 10-loops/03-while-loops/spec.js
@@ -1,77 +1,68 @@
 var expect = require('chai').expect;
-var solution = require('./challenge');
+var challenge = require('./challenge');
 var _ = require('underscore');
 
-var hasElements = function(collection, elements) {
-  for (var j = 0; j < elements.length; j++) {
-    var foundElem = false;
-    for (var i = 0; i < collection.length; i++) {
-      if (collection[i] == elements[j]) {
-        foundElem = true;
-        break;
-      }
-    }
-
-    if (!foundElem) {
-      return false;
-    }
-  }
-  return true;
-};
-
-
-describe("getKeys", function() {
-  it("should return an array of all the keys", function() {
-    hash = {name: "Rob", age: 101};
-    array = ["name", "age"];
-    result = solution.getKeys(hash);
-
-    expect(result).to.include('name');
-    expect(result).to.include('age');
+describe("stream", function() {
+  it("should be defined", function() {
+    expect(challenge.stream).to.exist;
   });
 
-  it("should return an empty array when the object is empty", function() {
-    expect(solution.getKeys({})).to.eql([]);
-  });
-});
-
-describe("getValues", function() {
-  it("should return an array of all the keys", function() {
-    hash = {name: "Rob", age: 101};
-    array = ["Rob", 101];
-    result = solution.getValues(hash);
-
-    expect(result).to.include('Rob');
-    expect(result).to.include(101);
+  it("shouldn't call actionFn when conditionalFn returns false immediately", function() {
+    var conditionalFn = function() { return false; };
+    var actionFn = function() { throw new Error("actionFn shouldn't have been called"); };
+    expect(challenge.stream(conditionalFn, actionFn)).to.not.throw;
   });
 
-  it("should return an empty array when the object is empty", function() {
-    expect(solution.getValues({})).to.eql([]);
+  it("should call actionFn once conditionalFn when returns true once", function() {
+    var flag = true;
+    var array = [];
+    var conditionalFn = function() {
+      var oldFlag = flag;
+      flag = !flag;
+      return oldFlag; 
+    };
+    var actionFn = function() {
+      array.push('1');
+    };
+    var result = challenge.stream(conditionalFn, actionFn);
+    expect(array).to.have.length(1, "actionFn should have been called once.")
+  });
+
+  it("should call actionFn 10 times ", function() {
+    var counter = 10;
+    var array = [];
+    var conditionalFn = function() {
+      counter--;
+      return counter >= 0;
+    };
+    var actionFn = function() {
+      array.push('1');
+    };
+    var result = challenge.stream(conditionalFn, actionFn);
+    expect(array).to.have.length(10, "actionFn should have been called 10 times.")
   });
 });
 
 
-describe("objectToArray", function() {
-  it("should convert a hash into an array of strings", function() {
-    hash = { name: "Bob", age: 34 }
-    array = [ "name is Bob", "age is 34" ]
-
-    result = solution.objectToArray(hash);
-    expect(result).to.include("name is Bob");
-    expect(result).to.include("age is 34");
+describe("sumNumbers", function() {
+  it("should be defined", function() {
+    expect(challenge.sumNumbers).to.exist;
   });
 
-  it("returns array for longer hash", function() {
-    hash = { name: "Joe", age: 34, sex: 'male' }
-    array = [ "name is Joe", "age is 34", "sex is male" ]
-    result = solution.objectToArray(hash);
-    expect(result).to.include("name is Joe");
-    expect(result).to.include("age is 34");
-    expect(result).to.include("sex is male");
+  it("sums consecutive numbers", function() {
+    expect(challenge.sumNumbers([1,2,3])).to.eql(6);
   });
 
-  it("returns an empty array when the hash is empty", function() {
-    expect(solution.objectToArray([])).to.eql([]);
+  it("sums random numbers", function() {
+    expect(challenge.sumNumbers([5, 23, 4])).to.eql(32);
+  });
+
+  it("can be called multiple times and return the correct result.", function() {
+    expect(challenge.sumNumbers([5, 23, 4])).to.eql(32);
+    expect(challenge.sumNumbers([5, 23, 4, 1])).to.eql(33);
+  });
+
+  it("returns 0 for an empty array", function() {
+    expect(challenge.sumNumbers([])).to.eql(0);
   });
 });
-
